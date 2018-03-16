0f1f9f814d45285673d1cccd49035e46b82fc99d
EC2 Default User <ec2-user@ip-172-31-35-180.us-east-2.compute.internal>
2018-03-16 11:54:01 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,7 +4,7 @@ class TasksController < ApplicationController
   end
   
   def show
-    @task = Task.find(params[:id])
+    set_task
   end
   
   def new
@@ -24,12 +24,11 @@ class TasksController < ApplicationController
   end
   
   def edit
-    @task = Task.find(params[:id])
+    set_task
   end
   
   def update
-    @task = Task.find(params[:id])
-    
+    set_task
     if @task.update(task_params)
       flash[:task] = 'Taskは正常に更新されました'
       redirect_to @task
@@ -40,7 +39,7 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @task = Task.find(params[:id])
+    set_task
     @task.destroy
     
     flash[:success] ='正常に削除されました'
@@ -48,6 +47,11 @@ class TasksController < ApplicationController
   end
   
   private
+  
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
   
   #strong Parameter
   def task_params
